afcc16128fe57895bdf87fa645fd9759cc40abc6
Sergey Rykov <sergrykov89@gmail.com>
2022-04-10 12:18:50 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("100100", "1111011"));
+        System.out.println(binOps.mult("101100", "1100"));
+    }
+}
